7965fba0c5f1aa537cae6f2632c3c5fb643cc71f
Dias1c <dias.kappasov@gmail.com>
2025-01-18 18:47:59 +0500

    fix(02-write-file): add graceful shutdown

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,25 @@
-const { stdin } = require('process');
+const process = require('process');
 const path = require('path');
 const fs = require('fs');
+const stdin = process.stdin;
 
 const fileName = path.join(__dirname, 'text.txt');
 
+const breakListeningWithMessage = () => {
+  console.log('Bye');
+  stdin.destroy();
+};
+
+process.on('SIGINT', () => breakListeningWithMessage());
+
 stdin.on('data', (chunk) => {
-  fs.appendFile(fileName, chunk.toString(), (err) => {
+  const text = chunk.toString();
+  if (text == 'exit\n') {
+    breakListeningWithMessage();
+    return;
+  }
+
+  fs.appendFile(fileName, text, (err) => {
     if (!err) {
       return;
     }
